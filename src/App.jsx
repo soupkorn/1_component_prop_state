a4193c4a8c1914296aeac50cfc819ddf3fd6fa4a
Your Name <you@example.com>
2025-09-19 14:45:53 +0700

    first prop!

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
-
 import reactImg from "./assets/react-core-concepts.png";
-const reactDescription = ['Fundamental', 'Crucial', 'Core']
+import componentsImg from "./assets/components.png";
 
-function getRandomInt(max){
-  return Math.floor(Math.random()*(max));
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
-function Header(){
+function Header() {
   const description = reactDescription[getRandomInt(reactDescription.length)];
   return (
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept({ title, description, img }) {
+  return (
+    <li>
+      <img src={img} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
   );
 }
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <CoreConcept
+            title="Components"
+            description="The core UI building block"
+            img={componentsImg}
+          />
+        </section>
+        "<h2>Time to get started!</h2>
       </main>
     </div>
-    );
+  );
 }
 
 export default App;
