9b950dfc75fccfd668da2ed96c3ac4caa200a734
Your Name <you@example.com>
2025-09-19 14:34:36 +0700

    create first component and variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-function App() {
+
+import reactImg from "./assets/react-core-concepts.png";
+const reactDescription = ['Fundamental', 'Crucial', 'Core']
+
+function getRandomInt(max){
+  return Math.floor(Math.random()*(max));
+}
+
+function Header(){
+  const description = reactDescription[getRandomInt(reactDescription.length)];
   return (
-    <div>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header/>
       <main>
         <h2>Time to get started!</h2>
       </main>
     </div>
-  );
+    );
 }
 
 export default App;
